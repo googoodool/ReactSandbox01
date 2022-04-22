c5e1e3732b98b3373fba9f0c174d15c9e62dc525
googoodool <47731412+googoodool@users.noreply.github.com>
2022-04-22 22:11:38 +0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Column cartoon</h1>
+    <p>This is paragraph</p>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
