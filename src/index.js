a015e60d406a2cb245c63be20cf4063489e63106
googoodool <47731412+googoodool@users.noreply.github.com>
2022-05-02 16:19:50 +0700

    Learning style

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+let status = "Good morning";
+const d = new Date(2022, 4, 23, 8);
+let hour = d.getHours();
+
+const sty = {
+  color: "red",
+  fontSize: "70px"
+};
+if (hour >= 12 && hour < 18) {
+  status = "Good afternoon";
+  sty.color = "green";
+} else if (hour >= 18 && hour <= 23) {
+  status = "Good Evening";
+  sty.color = "orange";
+}
+
 ReactDom.render(
-  <div>
-    <h1>Column cartoon</h1>
-    <p>This is paragraph</p>
-    <ul>
-      <li>Coffee</li>
-      <li>Tea</li>
-      <li>Milk</li>
-    </ul>
-  </div>,
+  <h1 style={sty}>
+    {status} : {hour}
+  </h1>,
   document.getElementById("root")
 );
